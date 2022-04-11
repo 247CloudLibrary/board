0cd0c8517da5f67c392f159060ae8bcd0f8b3c2a
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-11 20:42:28 +0900

    [update] BoardOperationUseCase

--- src/main/java/com/cloudlibrary/board/application/service/BoardOperationUseCase.java
+++ src/main/java/com/cloudlibrary/board/application/service/BoardOperationUseCase.java
@@ -1,4 +1,49 @@
 package com.cloudlibrary.board.application.service;
 
+import com.cloudlibrary.board.infrastructure.persistence.mysql.entity.BoardType;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 public interface BoardOperationUseCase {
+
+    void createBoard(BoardCreateCommand command);
+    BoardReadUseCase.FindBoardResult updateBoard(BoardUpdateCommand command);
+    void deleteBoard(BoardDeleteCommand command);
+
+    @EqualsAndHashCode(callSuper = false)
+    @Builder
+    @Getter
+    @ToString
+    class BoardCreateCommand {
+        private final long adminId;
+        private final String adminName;
+        private final String type;
+        private final String title;
+        private final String contents;
+        private final String libraryName;
+    }
+
+    @EqualsAndHashCode(callSuper = false)
+    @Builder
+    @Getter
+    @ToString
+    class BoardUpdateCommand {
+        private final long adminId;
+        private final String adminName;
+        private final String type;
+        private final String title;
+        private final String contents;
+        private final String libraryName;
+
+    }
+
+    @EqualsAndHashCode(callSuper = false)
+    @Builder
+    @Getter
+    @ToString
+    class BoardDeleteCommand {
+        private final long id;
+    }
 }
