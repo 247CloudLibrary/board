ac23bfb0834c72b47ff059bfa24d46a397eafee8
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-11 20:46:04 +0900

    [update] null체크 validation 추가

--- src/main/java/com/cloudlibrary/board/ui/requestBody/BoardCreateRequest.java
+++ src/main/java/com/cloudlibrary/board/ui/requestBody/BoardCreateRequest.java
@@ -1,18 +1,23 @@
 package com.cloudlibrary.board.ui.requestBody;
 
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class BoardCreateRequest {
+
     private String type;
+    @NotEmpty(message="제목은 필수입니다")
     private String title;
+    @NotEmpty(message = "내용은 필수입니다")
     private String contents;
+
     private String libraryName;
 }
--- src/main/java/com/cloudlibrary/board/ui/requestBody/BoardUpdateRequest.java
+++ src/main/java/com/cloudlibrary/board/ui/requestBody/BoardUpdateRequest.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @ToString
@@ -12,7 +14,12 @@ import lombok.ToString;
 public class BoardUpdateRequest {
 
     private String type;
+
+    @NotEmpty(message="제목은 필수입니다")
     private String title;
+
+    @NotEmpty(message = "내용은 필수입니다")
     private String contents;
+
     private String libraryName;
 }
