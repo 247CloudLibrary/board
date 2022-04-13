fef0715f3ef74cefb94dcc0e3b44a2233b379524
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-13 20:01:03 +0900

    [update] 크로스도메인 이슈

--- src/main/java/com/cloudlibrary/board/ui/controller/BoardController.java
+++ src/main/java/com/cloudlibrary/board/ui/controller/BoardController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "*")
 @Slf4j
 @RestController
 @Api(value="보드 API")
